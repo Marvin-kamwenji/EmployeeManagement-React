576e26c004d6f02d3da978492c6636816f88b456
Marvin-kamwenji <marvinkamwenjih@gmail.com>
2022-01-05 11:29:49 +0300

    Added Service and uses axios to communicate with the server side

--- src/components/ListEmployeeComponent.jsx
+++ src/components/ListEmployeeComponent.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import EmployeeService from '../services/EmployeeService';
 
 class ListEmployeeComponent extends Component {
   
@@ -8,7 +9,13 @@ class ListEmployeeComponent extends Component {
                 employees: []
             }
         }
-      
+    
+    componentDidMount(){
+    EmployeeService.getEmployees().then((res) => {
+      this.setState({employees: res.data});
+    });
+    }   
+
     render() {
         return (
             <div>
